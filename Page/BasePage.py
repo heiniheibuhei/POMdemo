3e88eee5a9c08c0f8507489d2e8c9e377c292fd6
1090482349@qq.com <1090482349@qq.com>
2019-07-14 22:33:10 +0800

    cuijs test111

--- Page/BasePage.py
+++ Page/BasePage.py
@@ -1,7 +1,10 @@
 # coding=utf-8
-# _author_:cuijs
 # date:2019-06-27
 import unittest
+from appium import webdriver
+import time
+from Public.log import logger
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class Action(unittest.TestCase):
@@ -9,7 +12,7 @@ class Action(unittest.TestCase):
     Base封装公共方法。drive、caps
     """
     def setUp(self):
-        print("--------------开始执行用例------------------6")
+        print("--------------开始执行用例------------------")
 
     def tearDown(self):
         print("--------------用例执行结束-----------------")
@@ -21,31 +24,34 @@ class Action(unittest.TestCase):
             'deviceName': '4695e5887d14',
             'platformVersion': '6.0',
             # apk包名
-            'appPackage': 'com.wondershare.drfone',
+            'appPackage': 'com.taobao.taobao',
             # apk的launcherActivity
-            'appActivity': 'com.wondershare.drfone.ui.activity.Main2Activity',
-            'noReset': 'false'
+            'appActivity': 'com.taobao.tao.TBMainActivity',
+            'noReset': 'false',
+            'resetKeyboard': 'true',
+            'unicodeKeyboard': 'true'
         }
         cls.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
-        logging.info('start run app...')
+        logger.info('--------start app ...----------')
 
-    def tearDownClass(self):
+    @classmethod
+    def tearDownClass(cls):
         cls.driver.quit()
 
     def find_element(self, loc):
         try:
-            webDriverWait(self.driver, 15).until(lambda driver:
+            WebDriverWait(self.driver, 15).until(lambda driver:
                                                  driver.find_element(*loc).is_displaued())
             return self.driver.find_element(*loc)
         except:
-            print("%s页面中未能找到%s元素"%(self, loc))
+            print("%s页面中未能找到%s元素" % (self, loc))
 
     def clear_keys(self, loc):
         # 重写清空输入框
         time.sleep(1)
         self.find_element(loc).clear()
 
-    def send_keys(self, loc ,value):
+    def send_keys(self, loc, value):
         # 重写输入框输入内容
         self.clear_keys(loc)
         self.find_element(loc).send_keys(value)
@@ -54,6 +60,13 @@ class Action(unittest.TestCase):
         # 重写点击按钮
         self.find_element(loc).click()
 
+    def test_case1(self):
+        time.sleep(10)
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
+#     driver1 = Action()
+#     driver1.setUpClass()
+
+
--- Public/log.py
+++ Public/log.py
@@ -9,22 +9,24 @@ class Logger(object):
         self.log_file_name = 'test.log'
         # 日志输出级别
         self.console_output_level = 'INFO'
-        self.log_output_level = 'DEBUG'
+        self.file_output_level = 'INFO'
         # 日志输出格式
-        self.formatter = loging.Formatter(
+        self.formatter = logging.Formatter(
             '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
         )
 
     def get_logger(self):
-        '''封装logger'''
+        """封装logger"""
         if not self.logger.handlers:
             console_hangler = logging.StreamHandler()
             console_hangler.setFormatter(self.formatter)
-            console_hangler.setForLevel(self.console_output_level)
+            console_hangler.setLevel(self.console_output_level)
             self.logger.addHandler(console_hangler)
-            file_handler = logging.FileHandler(self.formatter)
+            file_handler = logging.FileHandler(self.log_file_name)
             file_handler.setFormatter(self.formatter)
-            file_handler.setLevel(self.log_file_level)
+            file_handler.setLevel(self.file_output_level)
             self.logger.addHandler(file_handler)
             return self.logger
+
+
 logger = Logger().get_logger()
